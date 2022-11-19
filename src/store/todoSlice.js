b69dd1bf7697198ab3c843e30eebf89e3f0ca9ea
Schleidens <alectineschleidens2016@gmail.com>
2022-11-19 15:39:18 -0500

    starting wit redux by creating the store.js and todoSlice, create the skeleton for the code

---
+++ src/store/todoSlice.js
@@ -0,0 +1,21 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const todoSlice = createSlice({
+    name: 'todos',
+    initialState: [
+    ],
+    reducers: {
+        addTodo: (state, action)=>{
+            const todo = {
+                id: Math.floor(Math.random() * 10),
+                title: action.payload.title,
+                completed: false
+            };
+            state.push(todo);
+        },
+    },
+});
+
+
+export const {addTodo} = todoSlice.actions;
+export default todoSlice.reducer;
